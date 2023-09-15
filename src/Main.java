825abd5d9ce44e4025066b89223d78c2dc822a44
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-15 09:08:07 -0400

    great tax

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variable declarations
+        double price;
+        double sales;
+        double tax = .05;
+        //processing
+        System.out.println("Please enter the price of your item here");
+        price = scan.nextDouble();
+        sales = price * 1.05;
+        System.out.println("The price of your item after tax is " + sales + " and the price of your tax is " + (price * .05) + ".");
+
     }
 }
